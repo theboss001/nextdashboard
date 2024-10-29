40c1071da6c6498976a67f59ff31de257b74d6fb
misterJS <landrykolai700@gmail.com>
2024-10-29 00:23:28 +0000

    an updated file

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,7 +5,7 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
+// export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
 
 //   const allPages = generatePagination(currentPage, totalPages);
